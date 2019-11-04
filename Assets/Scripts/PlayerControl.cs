ad04592eb0dc8a63b4ad1f7ee7551259c207c329
AidenTheEditor <56165455+AidenTheEditor@users.noreply.github.com>
2019-11-04 07:33:40 -0800

    Step-2,3,4
    I'm Didn't commit step-2 at the right time so i'm commiting it now with step-3 and step 4 already completed with it.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private GameObject veh_Ute_Red_Z;
+    public Vector3 offset = new Vector3(0, 5, -7);
+    public float speed = 5.0f;
+    private float turnspeed = 5.0f;
+    public float horizontallinput;
+    public float forwardInput;
+
+    public float Turnspeed { get => turnspeed; set => turnspeed = value; }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontallinput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontallinput);
+
+    }
+}
